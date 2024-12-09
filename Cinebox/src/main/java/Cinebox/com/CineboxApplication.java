386689abd2dc12ed0f795a1357aab69081083741
[Alejandro Jose Garcia Chaname] <[chana_15@outlook.com]>
2024-12-09 01:16:28 -0500

    eureka

--- Cinebox/src/main/java/Cinebox/com/CineboxApplication.java
+++ Cinebox/src/main/java/Cinebox/com/CineboxApplication.java
@@ -2,8 +2,10 @@ package Cinebox.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CineboxApplication {
 
 	public static void main(String[] args) {
